3c614a24dc17252a54ab8aa9cd220fc0aec7e757
dajiangjunok <779503577@qq.com>
2022-07-28 20:51:11 +0800

    chore: run tests on travis ci

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,17 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module '*.css' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.scss' {
+  const value: any;
+  export = value;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import viteEslint from 'vite-plugin-eslint';
+// stylelint插件
+import viteStylelint from '@amatlash/vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    viteEslint(),
+    viteStylelint({
+      // 对某些文件排除检查
+      exclude: /windicss|node_modules/
+    })
+  ]
+});
